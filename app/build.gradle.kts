d55cd5225ed1821bc6176ba17978a853481e50ce
whiitekniight <whiitekniight71@gmail.com>
2026-06-08 20:09:35 -0500

    Release branded update 1.9.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.example.greenstreem"
         minSdk = 23
         targetSdk = 36
-        versionCode = 50
-        versionName = "1.9.14"
+        versionCode = 51
+        versionName = "1.9.15"
     }
 
     flavorDimensions += "distribution"
